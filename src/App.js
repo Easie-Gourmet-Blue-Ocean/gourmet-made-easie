8109e30ed99c59770fba84427465ea91cc80d007
MatthewZipkes <matthewzipkes@gmail.com>
2021-12-07 12:21:08 -0600

    react app setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { hot } from 'react-hot-loader/root';
+
+const App = () => {
+    return (
+        <>
+        <h1>
+            Hello World
+        </h1>
+        </>
+    )
+}
+
+export default hot(App);
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: [
+    'react-hot-loader/patch',
+    './src/index.js'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  devServer: {
+    'static': {
+      directory: './dist'
+    }
+  }
+};
+
+module.exports = config;
