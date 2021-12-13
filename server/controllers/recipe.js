17d37b625c3f4c0260b81f388cbcac1723157d28
MatthewZipkes <matthewzipkes@gmail.com>
2021-12-13 12:38:13 -0600

    postgreSQL setup

---
+++ server/controllers/recipe.js
@@ -0,0 +1,24 @@
+/*
+GET /recipe/:recipeId
+GET /recipe/cards
+POST /recipe
+*/
+const recipeModel = require('../models/recipe')
+
+const getDetailedRecipes = (req, res) => {
+  recipeModel.addRecipe
+}
+
+const getRecipeCards = (req, res) => {
+  
+}
+
+const postRecipe = (req, res) => {
+  
+}
+
+module.exports = {
+    getDetailedRecipes,
+    getRecipeCards,
+    postRecipe
+}
