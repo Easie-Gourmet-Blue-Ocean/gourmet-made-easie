daf2253495efba0723f24d3f9321c8459e9e8737
Sam Martin <martin110sam@gmail.com>
2021-12-14 13:58:01 -0600

    searchbar complete - not styled

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import img from "../../assets/images/bg.jpeg";
+import Searchbar from "../../sharedComponents/Searchbar.js";
 const Home = () => {
+  const [searchInput, setSearchInput] = "";
+  console.log(setSearchInput);
+  var onChangeHandler = (e) => {
+    setSearchInput(e.target.value);
+  };
+
   return (
-    <div className="home-container">
+    <div className="header-container">
+        <div className="home-search-container">
+          <div className="search-title">What do you want to eat?</div>
+          <Searchbar />
+          <div className="random-recipe-button">
+            <button>Random recipe</button>
+          </div>
+        </div>
       <div className="image">
         <img src={img}></img>
-        <div className="color-box"></div>
       </div>
     </div>
   );
--- src/sharedComponents/Searchbar.js
+++ src/sharedComponents/Searchbar.js
@@ -1,10 +1,26 @@
 import React from "react";
 
 const Searchbar = () => {
-    return (
-        <>
-        </>
-    )
-}
+  const [searchTerm, setSearchTerm] = React.useState("");
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  return (
+    <div className="searchbar-container">
+        <input
+        className="searchbar"
+        type="text"
+        placeholder="Search"
+        value={searchTerm}
+        onChange={handleChange}
+        />
+        <div className="search-submit">
+        <button>Submit</button>
+        </div>
+    </div>
+  );
+};
 
 export default Searchbar;
