57e62dbafdb742b6ac058a6c8a6a38ce5b802a72
Sam Martin <martin110sam@gmail.com>
2021-12-15 01:02:20 -0600

    Added all top recipe buttons

--- src/pages/Home/components/TopRecipes.js
+++ src/pages/Home/components/TopRecipes.js
@@ -8,34 +8,49 @@ const TopRecipes = () => {
       <div className="tr-list">
         <div className="tr-type">
           <div className="title-button-row">
-          <h3>Dinner</h3>
-          <button>See More &#8594;</button>
+            <h3>Dinner</h3>
+            <button>See More &#8594;</button>
           </div>
           <RecipeCard />
           <RecipeCard />
         </div>
         <div className="tr-type">
-        <h3>Lunch</h3>
+          <div className="title-button-row">
+            <h3>Lunch</h3>
+            <button>See More &#8594;</button>
+          </div>
           <RecipeCard />
           <RecipeCard />
         </div>
         <div className="tr-type">
-        <h3>Breakfast</h3>
+          <div className="title-button-row">
+            <h3>Breakfast</h3>
+            <button>See More &#8594;</button>
+          </div>
           <RecipeCard />
           <RecipeCard />
         </div>
         <div className="tr-type">
-          <h3>Dessert</h3>
+          <div className="title-button-row">
+            <h3>Dessert</h3>
+            <button>See More &#8594;</button>
+          </div>
           <RecipeCard />
           <RecipeCard />
         </div>
         <div className="tr-type">
-        <h3>Appetizers</h3>
+          <div className="title-button-row">
+            <h3>Appetizers</h3>
+            <button>See More &#8594;</button>
+          </div>
           <RecipeCard />
           <RecipeCard />
         </div>
         <div className="tr-type">
-        <h3>Brunch</h3>
+          <div className="title-button-row">
+            <h3>Brunch</h3>
+            <button>See More &#8594;</button>
+          </div>
           <RecipeCard />
           <RecipeCard />
         </div>
