bb076e67fe1b34dc91819edca02bd08178a47715
Patrick Lorden <pjlorden@gmail.com>
2021-12-16 13:46:21 -0600

    impleneted basic user page functionality, unable to test until login complete

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import RecipeList from '../../sharedComponents/RecipeList.js';
 
+
+// will need a redirect to add a recipe
+// will need to get users recipe cards and a favorite cards
 const Profile = () => {
+
+    const [userRecipes, setUserRecipes] = useState([]);
+    const [favoriteRecipes, setFavoriteRecipes] = useState([]);
+    //True means show userRecipes, false means showFavorite Recipes
+    const [recipeSwitch, setRecipeSwitch] = useState(true);
+
     return (
         <>
+        {/* FOR SAM:Possibly change the tag for the profile while styling */}
+        <h1>My Profile</h1>
+        <button type='button'>Add Recipe</button>
+        <button type='button' onClick={() => setRecipeSwitch(true)}>My Recipes</button>
+        <button type='button' onClick={() => setRecipeSwitch(false)}>My Favorites</button>
+        <RecipeList recipes={recipeSwitch ? userRecipes : favoriteRecipes}/>
         </>
     )
 }
