7c5bbbff20b456abc195ab61b33d50e2770e2554
Patrick Lorden <pjlorden@gmail.com>
2021-12-16 13:24:22 -0600

    added Sam's shared card and list components

--- src/pages/Home/components/TopRecipes.js
+++ src/pages/Home/components/TopRecipes.js
@@ -1,7 +1,18 @@
 import React from "react";
 import RecipeCard from "../../../sharedComponents/RecipeCard.js";
+import RecipeList from "../../../sharedComponents/RecipeList.js";
 
 const TopRecipes = () => {
+
+
+  const [dinnerRecipes, setDinnerRecipes] = React.useState([]);
+  const [lunchRecipes, setLunchRecipes] = React.useState([]);
+  const [breakfastRecipes, setBreakfastRecipes] = React.useState([]);
+  const [dessertRecipes, setDessertRecipes] = React.useState([]);
+  const [appetizerRecipes, setAppetizerRecipes] = React.useState([]);
+  const [brunchRecipes, setBrunchRecipes] = React.useState([]);
+
+
   return (
     <div className="top-recipes-container">
       <h2 className="tr-title">Today's Top Recipes</h2>
@@ -11,48 +22,42 @@ const TopRecipes = () => {
             <h3>Dinner</h3>
             <button>See More &#8594;</button>
           </div>
-          <RecipeCard />
-          <RecipeCard />
+          <RecipeList value={dinnerRecipes}/>
         </div>
         <div className="tr-type">
           <div className="title-button-row">
             <h3>Lunch</h3>
             <button>See More &#8594;</button>
           </div>
-          <RecipeCard />
-          <RecipeCard />
+          <RecipeList value={lunchRecipes}/>
         </div>
         <div className="tr-type">
           <div className="title-button-row">
             <h3>Breakfast</h3>
             <button>See More &#8594;</button>
           </div>
-          <RecipeCard />
-          <RecipeCard />
+          <RecipeList value={breakfastRecipes}/>
         </div>
         <div className="tr-type">
           <div className="title-button-row">
             <h3>Dessert</h3>
             <button>See More &#8594;</button>
           </div>
-          <RecipeCard />
-          <RecipeCard />
+          <RecipeList value={dessertRecipes}/>
         </div>
         <div className="tr-type">
           <div className="title-button-row">
             <h3>Appetizers</h3>
             <button>See More &#8594;</button>
           </div>
-          <RecipeCard />
-          <RecipeCard />
+          <RecipeList value={appetizerRecipes}/>
         </div>
         <div className="tr-type">
           <div className="title-button-row">
             <h3>Brunch</h3>
             <button>See More &#8594;</button>
           </div>
-          <RecipeCard />
-          <RecipeCard />
+          <RecipeList value={brunchRecipes}/>
         </div>
       </div>
     </div>
--- src/sharedComponents/RecipeCard.js
+++ src/sharedComponents/RecipeCard.js
@@ -1,15 +1,29 @@
 import React from "react";
+import RecipeList from "../sharedComponents/RecipeList.js";
+// NEED AXIOS DATA
 
-const RecipeCard = () => {
+const RecipeCard = (recipe) => {
+
+    let photo = recipe.value.photo;
+    let recipeName = recipe.value.recipeName.charAt(0).toUpperCase() + recipe.value.recipeName.slice(1);;
+    let username = recipe.value.userName.charAt(0).toUpperCase() + recipe.value.userName.slice(1);
+    let description = recipe.value.description.charAt(0).toUpperCase() + recipe.value.description.slice(1);
 
     return (
-       <div className="recipe-card">
-           <div className="rc-image">image</div>
-           <div className="rc-info">
-               <div className="rc-name">Recipe Name by "username"</div>
-           </div>
-       </div>
-    )
-}
+      <div className="recipe-card-container" >
+        <div className="rc-image">
+            <img src={photo}></img>
+        </div>
+        <div className="rc-info">
+          <div className="rc-info-top">
+            <div className="rc-recipe-name">{recipeName}</div>
+            <div className="rc-username">by: {username}</div>
+          </div>
+          <div className="rc-info-bottom">{description}</div>
+        </div>
+      </div>
+    );
+
+  };
 
 export default RecipeCard;
