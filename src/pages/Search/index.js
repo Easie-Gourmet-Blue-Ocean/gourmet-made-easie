57e3d5d88c14f6cd0dbc3f9d9f346fa0a3edd3ff
Patrick Lorden <pjlorden@gmail.com>
2021-12-15 22:55:05 -0600

    implemented sort page before recipe list and tile functionality

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
+import Searchbar from '../../sharedComponents/Searchbar.js';
+import SortMenu from '../../sharedComponents/SortMenu.js';
 
 const Search = () => {
+
+ const [status, setStatus] = useState('');
+ const [recipes, setRecipes] = useState([{}]);
+
+
+
     return (
         <>
+            <Searchbar/>
+            <SortMenu setStateHook={setStatus} />
         </>
     )
 }
