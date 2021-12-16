418aab8c8064ed60cdc147626647c4f7ba553fe2
Patrick Lorden <pjlorden@gmail.com>
2021-12-16 13:30:08 -0600

    refactored recipeList and Card to function with data provided

--- src/pages/Home/components/TopRecipes.js
+++ src/pages/Home/components/TopRecipes.js
@@ -5,12 +5,84 @@ import RecipeList from "../../../sharedComponents/RecipeList.js";
 const TopRecipes = () => {
 
 
-  const [dinnerRecipes, setDinnerRecipes] = React.useState([]);
-  const [lunchRecipes, setLunchRecipes] = React.useState([]);
-  const [breakfastRecipes, setBreakfastRecipes] = React.useState([]);
-  const [dessertRecipes, setDessertRecipes] = React.useState([]);
-  const [appetizerRecipes, setAppetizerRecipes] = React.useState([]);
-  const [brunchRecipes, setBrunchRecipes] = React.useState([]);
+  const [dinnerRecipes, setDinnerRecipes] = React.useState([{
+    recipeName: "chicken and rice",
+    userName: "tester1",
+    description: "this is a great dish that I make after the gym",
+    photo:
+      "https://media.istockphoto.com/photos/roasted-whole-french-chicken-picture-id1192828034?k=20&m=1192828034&s=612x612&w=0&h=TUGYWNtQVV2DxjD1hKxHXbhAzj8s8bgupdx0RMo3EDo=",
+  },
+  {
+    recipeName: "chicken and rice",
+    userName: "tester1",
+    description: "this is a great dish that I make after the gym",
+    photo: "",
+  },]);
+  const [lunchRecipes, setLunchRecipes] = React.useState([{
+    recipeName: "chicken and rice",
+    userName: "tester1",
+    description: "this is a great dish that I make after the gym",
+    photo:
+      "https://media.istockphoto.com/photos/roasted-whole-french-chicken-picture-id1192828034?k=20&m=1192828034&s=612x612&w=0&h=TUGYWNtQVV2DxjD1hKxHXbhAzj8s8bgupdx0RMo3EDo=",
+  },
+  {
+    recipeName: "chicken and rice",
+    userName: "tester1",
+    description: "this is a great dish that I make after the gym",
+    photo: "",
+  },]);
+  const [breakfastRecipes, setBreakfastRecipes] = React.useState([{
+    recipeName: "chicken and rice",
+    userName: "tester1",
+    description: "this is a great dish that I make after the gym",
+    photo:
+      "https://media.istockphoto.com/photos/roasted-whole-french-chicken-picture-id1192828034?k=20&m=1192828034&s=612x612&w=0&h=TUGYWNtQVV2DxjD1hKxHXbhAzj8s8bgupdx0RMo3EDo=",
+  },
+  {
+    recipeName: "chicken and rice",
+    userName: "tester1",
+    description: "this is a great dish that I make after the gym",
+    photo: "",
+  },]);
+  const [dessertRecipes, setDessertRecipes] = React.useState([{
+    recipeName: "chicken and rice",
+    userName: "tester1",
+    description: "this is a great dish that I make after the gym",
+    photo:
+      "https://media.istockphoto.com/photos/roasted-whole-french-chicken-picture-id1192828034?k=20&m=1192828034&s=612x612&w=0&h=TUGYWNtQVV2DxjD1hKxHXbhAzj8s8bgupdx0RMo3EDo=",
+  },
+  {
+    recipeName: "chicken and rice",
+    userName: "tester1",
+    description: "this is a great dish that I make after the gym",
+    photo: "",
+  },]);
+  const [appetizerRecipes, setAppetizerRecipes] = React.useState([{
+    recipeName: "chicken and rice",
+    userName: "tester1",
+    description: "this is a great dish that I make after the gym",
+    photo:
+      "https://media.istockphoto.com/photos/roasted-whole-french-chicken-picture-id1192828034?k=20&m=1192828034&s=612x612&w=0&h=TUGYWNtQVV2DxjD1hKxHXbhAzj8s8bgupdx0RMo3EDo=",
+  },
+  {
+    recipeName: "chicken and rice",
+    userName: "tester1",
+    description: "this is a great dish that I make after the gym",
+    photo: "",
+  },]);
+  const [brunchRecipes, setBrunchRecipes] = React.useState([{
+    recipeName: "chicken and rice",
+    userName: "tester1",
+    description: "this is a great dish that I make after the gym",
+    photo:
+      "https://media.istockphoto.com/photos/roasted-whole-french-chicken-picture-id1192828034?k=20&m=1192828034&s=612x612&w=0&h=TUGYWNtQVV2DxjD1hKxHXbhAzj8s8bgupdx0RMo3EDo=",
+  },
+  {
+    recipeName: "chicken and rice",
+    userName: "tester1",
+    description: "this is a great dish that I make after the gym",
+    photo: "",
+  },]);
 
 
   return (
@@ -22,42 +94,42 @@ const TopRecipes = () => {
             <h3>Dinner</h3>
             <button>See More &#8594;</button>
           </div>
-          <RecipeList value={dinnerRecipes}/>
+          <RecipeList recipes={dinnerRecipes}/>
         </div>
         <div className="tr-type">
           <div className="title-button-row">
             <h3>Lunch</h3>
             <button>See More &#8594;</button>
           </div>
-          <RecipeList value={lunchRecipes}/>
+          <RecipeList recipes={lunchRecipes}/>
         </div>
         <div className="tr-type">
           <div className="title-button-row">
             <h3>Breakfast</h3>
             <button>See More &#8594;</button>
           </div>
-          <RecipeList value={breakfastRecipes}/>
+          <RecipeList recipes={breakfastRecipes}/>
         </div>
         <div className="tr-type">
           <div className="title-button-row">
             <h3>Dessert</h3>
             <button>See More &#8594;</button>
           </div>
-          <RecipeList value={dessertRecipes}/>
+          <RecipeList recipes={dessertRecipes}/>
         </div>
         <div className="tr-type">
           <div className="title-button-row">
             <h3>Appetizers</h3>
             <button>See More &#8594;</button>
           </div>
-          <RecipeList value={appetizerRecipes}/>
+          <RecipeList recipes={appetizerRecipes}/>
         </div>
         <div className="tr-type">
           <div className="title-button-row">
             <h3>Brunch</h3>
             <button>See More &#8594;</button>
           </div>
-          <RecipeList value={brunchRecipes}/>
+          <RecipeList recipes={brunchRecipes}/>
         </div>
       </div>
     </div>
--- src/sharedComponents/RecipeList.js
+++ src/sharedComponents/RecipeList.js
@@ -3,22 +3,7 @@ import RecipeCard from "../sharedComponents/RecipeCard.js";
 
 // NEED AXIOS
 
-const RecipeList = () => {
-  let recipes = [
-    {
-      recipeName: "chicken and rice",
-      userName: "tester1",
-      description: "this is a great dish that I make after the gym",
-      photo:
-        "https://media.istockphoto.com/photos/roasted-whole-french-chicken-picture-id1192828034?k=20&m=1192828034&s=612x612&w=0&h=TUGYWNtQVV2DxjD1hKxHXbhAzj8s8bgupdx0RMo3EDo=",
-    },
-    {
-      recipeName: "chicken and rice",
-      userName: "tester1",
-      description: "this is a great dish that I make after the gym",
-      photo: "",
-    },
-  ];
+const RecipeList = ({recipes}) => {
 
   return recipes.map((recipe, index) => {
       return <RecipeCard value={recipe} key={index} />;
